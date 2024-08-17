ce134085ef68e1df5f354d7e300c71b48ebcc0e4
mvishok <vishokmanikantan@gmail.com>
2024-08-17 19:22:21 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,13 @@
+export default function main(tag, strRender){
+    const till = parseInt(strRender(tag.attr('till'))) || 10;
+    const seperator = strRender(tag.attr('seperator')) || "<br>";
+    
+    let result = "";
+    for (let i = 0; i < till; i++){
+        if (i % 2 == 0){
+            result += i + seperator;
+        }
+    }
+
+    return result;
+}
