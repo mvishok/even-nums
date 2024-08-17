ee52141caa5cde5f32376f5f896d699eabf09607
Vishok M <89720170+mvishok@users.noreply.github.com>
2024-08-17 19:31:35 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 export default function main(tag, strRender){
     const till = parseInt(strRender(tag.attr('till'))) || 10;
-    const seperator = strRender(tag.attr('seperator')) || "<br>";
+    const seperator = strRender(tag.attr('sep')) || "<br>";
     
     let result = "";
     for (let i = 0; i < till; i++){
